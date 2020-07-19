80e702acd98218a15060c167486da760e99e0f51
albretost <64496370+albretost@users.noreply.github.com>
2020-07-19 22:39:53 +0700

    add argon

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,15 @@ Route::get('/galeri', function () {
 Route::get('/form', function () {
     return view('form.form');
 });
+
+Auth::routes();
+
+Route::get('/dashboard', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
+
